686b4977fda06a5da807e9cecc0cc18e3fa49cf0
Elschnagoo <david.nagy@quinto.io>
2021-10-24 14:42:46 +0200

    0.11.5 - Fix Pipeline + Jest Config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testRegex: "(/tests/*.test.ts|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  coverageReporters: ["html", "text", "text-summary", "cobertura"],
+  coverageReporters: ["html", "text", "text-summary", "cobertura", "lcov"],
   collectCoverageFrom: ["**/*.ts", "!**/node_modules/**","!tests/**"],
   testPathIgnorePatterns: ["/dist/", "/node_modules/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
